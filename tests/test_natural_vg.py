0d57c485a5232c924fe6bd0099aa2ba39af78aff
Carlos Bergillos <carlos.bergillos.varela@est.fib.upc.edu>
2023-01-21 22:08:49 +0200

    Formatting and new test

--- tests/test_natural_vg.py
+++ tests/test_natural_vg.py
@@ -31,7 +31,7 @@ def test_basic(sample_ts):
 
 
 def test_left_to_right(sample_ts):
-    out_got = ts2vg.NaturalVG(directed='left_to_right').build(sample_ts).edges
+    out_got = ts2vg.NaturalVG(directed="left_to_right").build(sample_ts).edges
 
     out_truth = [
         (0, 1),
@@ -44,137 +44,137 @@ def test_left_to_right(sample_ts):
 
 
 def test_left_to_right_distance(sample_ts):
-    out_got = ts2vg.NaturalVG(directed='left_to_right', weighted='distance').build(sample_ts).edges
+    out_got = ts2vg.NaturalVG(directed="left_to_right", weighted="distance").build(sample_ts).edges
 
     out_truth = [
-        (0, 1, approx(sqrt(2.))),
-        (1, 2, approx(sqrt(5.))),
-        (1, 3, approx(sqrt(13.))),
-        (2, 3, approx(sqrt(2.))),
+        (0, 1, approx(sqrt(2.0))),
+        (1, 2, approx(sqrt(5.0))),
+        (1, 3, approx(sqrt(13.0))),
+        (2, 3, approx(sqrt(2.0))),
     ]
 
     assert sorted(out_got) == sorted(out_truth)
 
 
 def test_left_to_right_sq_distance(sample_ts):
-    out_got = ts2vg.NaturalVG(directed='left_to_right', weighted='sq_distance').build(sample_ts).edges
+    out_got = ts2vg.NaturalVG(directed="left_to_right", weighted="sq_distance").build(sample_ts).edges
 
     out_truth = [
-        (0, 1, approx(2.)),
-        (1, 2, approx(5.)),
-        (1, 3, approx(13.)),
-        (2, 3, approx(2.)),
+        (0, 1, approx(2.0)),
+        (1, 2, approx(5.0)),
+        (1, 3, approx(13.0)),
+        (2, 3, approx(2.0)),
     ]
 
     assert sorted(out_got) == sorted(out_truth)
 
 
 def test_left_to_right_v_distance(sample_ts):
-    out_got = ts2vg.NaturalVG(directed='left_to_right', weighted='v_distance').build(sample_ts).edges
+    out_got = ts2vg.NaturalVG(directed="left_to_right", weighted="v_distance").build(sample_ts).edges
 
     out_truth = [
-        (0, 1, approx(1.)),
-        (1, 2, approx(-2.)),
-        (1, 3, approx(-3.)),
-        (2, 3, approx(-1.)),
+        (0, 1, approx(1.0)),
+        (1, 2, approx(-2.0)),
+        (1, 3, approx(-3.0)),
+        (2, 3, approx(-1.0)),
     ]
 
     assert sorted(out_got) == sorted(out_truth)
 
 
 def test_left_to_right_abs_v_distance(sample_ts):
-    out_got = ts2vg.NaturalVG(directed='left_to_right', weighted='abs_v_distance').build(sample_ts).edges
+    out_got = ts2vg.NaturalVG(directed="left_to_right", weighted="abs_v_distance").build(sample_ts).edges
 
     out_truth = [
-        (0, 1, approx(1.)),
-        (1, 2, approx(2.)),
-        (1, 3, approx(3.)),
-        (2, 3, approx(1.)),
+        (0, 1, approx(1.0)),
+        (1, 2, approx(2.0)),
+        (1, 3, approx(3.0)),
+        (2, 3, approx(1.0)),
     ]
 
     assert sorted(out_got) == sorted(out_truth)
 
 
 def test_left_to_right_h_distance(sample_ts):
-    out_got = ts2vg.NaturalVG(directed='left_to_right', weighted='h_distance').build(sample_ts).edges
+    out_got = ts2vg.NaturalVG(directed="left_to_right", weighted="h_distance").build(sample_ts).edges
 
     out_truth = [
-        (0, 1, approx(1.)),
-        (1, 2, approx(1.)),
-        (1, 3, approx(2.)),
-        (2, 3, approx(1.)),
+        (0, 1, approx(1.0)),
+        (1, 2, approx(1.0)),
+        (1, 3, approx(2.0)),
+        (2, 3, approx(1.0)),
     ]
 
     assert sorted(out_got) == sorted(out_truth)
 
 
 def test_left_to_right_abs_h_distance(sample_ts):
-    out_got = ts2vg.NaturalVG(directed='left_to_right', weighted='abs_h_distance').build(sample_ts).edges
+    out_got = ts2vg.NaturalVG(directed="left_to_right", weighted="abs_h_distance").build(sample_ts).edges
 
     out_truth = [
-        (0, 1, approx(1.)),
-        (1, 2, approx(1.)),
-        (1, 3, approx(2.)),
-        (2, 3, approx(1.)),
+        (0, 1, approx(1.0)),
+        (1, 2, approx(1.0)),
+        (1, 3, approx(2.0)),
+        (2, 3, approx(1.0)),
     ]
 
     assert sorted(out_got) == sorted(out_truth)
 
 
 def test_left_to_right_slope(sample_ts):
-    out_got = ts2vg.NaturalVG(directed='left_to_right', weighted='slope').build(sample_ts).edges
+    out_got = ts2vg.NaturalVG(directed="left_to_right", weighted="slope").build(sample_ts).edges
 
     out_truth = [
-        (0, 1, approx(1.)),
-        (1, 2, approx(-2.)),
+        (0, 1, approx(1.0)),
+        (1, 2, approx(-2.0)),
         (1, 3, approx(-1.5)),
-        (2, 3, approx(-1.)),
+        (2, 3, approx(-1.0)),
     ]
 
     assert sorted(out_got) == sorted(out_truth)
 
 
 def test_left_to_right_abs_slope(sample_ts):
-    out_got = ts2vg.NaturalVG(directed='left_to_right', weighted='abs_slope').build(sample_ts).edges
+    out_got = ts2vg.NaturalVG(directed="left_to_right", weighted="abs_slope").build(sample_ts).edges
 
     out_truth = [
-        (0, 1, approx(1.)),
-        (1, 2, approx(2.)),
+        (0, 1, approx(1.0)),
+        (1, 2, approx(2.0)),
         (1, 3, approx(1.5)),
-        (2, 3, approx(1.)),
+        (2, 3, approx(1.0)),
     ]
 
     assert sorted(out_got) == sorted(out_truth)
 
 
 def test_left_to_right_angle(sample_ts):
-    out_got = ts2vg.NaturalVG(directed='left_to_right', weighted='angle').build(sample_ts).edges
+    out_got = ts2vg.NaturalVG(directed="left_to_right", weighted="angle").build(sample_ts).edges
 
     out_truth = [
-        (0, 1, approx(atan(1.))),
-        (1, 2, approx(atan(-2.))),
+        (0, 1, approx(atan(1.0))),
+        (1, 2, approx(atan(-2.0))),
         (1, 3, approx(atan(-1.5))),
-        (2, 3, approx(atan(-1.))),
+        (2, 3, approx(atan(-1.0))),
     ]
 
     assert sorted(out_got) == sorted(out_truth)
 
 
 def test_left_to_right_abs_angle(sample_ts):
-    out_got = ts2vg.NaturalVG(directed='left_to_right', weighted='abs_angle').build(sample_ts).edges
+    out_got = ts2vg.NaturalVG(directed="left_to_right", weighted="abs_angle").build(sample_ts).edges
 
     out_truth = [
-        (0, 1, approx(atan(1.))),
-        (1, 2, approx(atan(2.))),
+        (0, 1, approx(atan(1.0))),
+        (1, 2, approx(atan(2.0))),
         (1, 3, approx(atan(1.5))),
-        (2, 3, approx(atan(1.))),
+        (2, 3, approx(atan(1.0))),
     ]
 
     assert sorted(out_got) == sorted(out_truth)
 
 
 def test_top_to_bottom(sample_ts):
-    out_got = ts2vg.NaturalVG(directed='top_to_bottom').build(sample_ts).edges
+    out_got = ts2vg.NaturalVG(directed="top_to_bottom").build(sample_ts).edges
 
     out_truth = [
         (1, 0),
@@ -187,137 +187,137 @@ def test_top_to_bottom(sample_ts):
 
 
 def test_top_to_bottom_distance(sample_ts):
-    out_got = ts2vg.NaturalVG(directed='top_to_bottom', weighted='distance').build(sample_ts).edges
+    out_got = ts2vg.NaturalVG(directed="top_to_bottom", weighted="distance").build(sample_ts).edges
 
     out_truth = [
-        (1, 0, approx(sqrt(2.))),
-        (1, 2, approx(sqrt(5.))),
-        (1, 3, approx(sqrt(13.))),
-        (2, 3, approx(sqrt(2.))),
+        (1, 0, approx(sqrt(2.0))),
+        (1, 2, approx(sqrt(5.0))),
+        (1, 3, approx(sqrt(13.0))),
+        (2, 3, approx(sqrt(2.0))),
     ]
 
     assert sorted(out_got) == sorted(out_truth)
 
 
 def test_top_to_bottom_sq_distance(sample_ts):
-    out_got = ts2vg.NaturalVG(directed='top_to_bottom', weighted='sq_distance').build(sample_ts).edges
+    out_got = ts2vg.NaturalVG(directed="top_to_bottom", weighted="sq_distance").build(sample_ts).edges
 
     out_truth = [
-        (1, 0, approx(2.)),
-        (1, 2, approx(5.)),
-        (1, 3, approx(13.)),
-        (2, 3, approx(2.)),
+        (1, 0, approx(2.0)),
+        (1, 2, approx(5.0)),
+        (1, 3, approx(13.0)),
+        (2, 3, approx(2.0)),
     ]
 
     assert sorted(out_got) == sorted(out_truth)
 
 
 def test_top_to_bottom_v_distance(sample_ts):
-    out_got = ts2vg.NaturalVG(directed='top_to_bottom', weighted='v_distance').build(sample_ts).edges
+    out_got = ts2vg.NaturalVG(directed="top_to_bottom", weighted="v_distance").build(sample_ts).edges
 
     out_truth = [
-        (1, 0, approx(-1.)),
-        (1, 2, approx(-2.)),
-        (1, 3, approx(-3.)),
-        (2, 3, approx(-1.)),
+        (1, 0, approx(-1.0)),
+        (1, 2, approx(-2.0)),
+        (1, 3, approx(-3.0)),
+        (2, 3, approx(-1.0)),
     ]
 
     assert sorted(out_got) == sorted(out_truth)
 
 
 def test_top_to_bottom_abs_v_distance(sample_ts):
-    out_got = ts2vg.NaturalVG(directed='top_to_bottom', weighted='abs_v_distance').build(sample_ts).edges
+    out_got = ts2vg.NaturalVG(directed="top_to_bottom", weighted="abs_v_distance").build(sample_ts).edges
 
     out_truth = [
-        (1, 0, approx(1.)),
-        (1, 2, approx(2.)),
-        (1, 3, approx(3.)),
-        (2, 3, approx(1.)),
+        (1, 0, approx(1.0)),
+        (1, 2, approx(2.0)),
+        (1, 3, approx(3.0)),
+        (2, 3, approx(1.0)),
     ]
 
     assert sorted(out_got) == sorted(out_truth)
 
 
 def test_top_to_bottom_h_distance(sample_ts):
-    out_got = ts2vg.NaturalVG(directed='top_to_bottom', weighted='h_distance').build(sample_ts).edges
+    out_got = ts2vg.NaturalVG(directed="top_to_bottom", weighted="h_distance").build(sample_ts).edges
 
     out_truth = [
-        (1, 0, approx(-1.)),
-        (1, 2, approx(1.)),
-        (1, 3, approx(2.)),
-        (2, 3, approx(1.)),
+        (1, 0, approx(-1.0)),
+        (1, 2, approx(1.0)),
+        (1, 3, approx(2.0)),
+        (2, 3, approx(1.0)),
     ]
 
     assert sorted(out_got) == sorted(out_truth)
 
 
 def test_top_to_bottom_abs_h_distance(sample_ts):
-    out_got = ts2vg.NaturalVG(directed='top_to_bottom', weighted='abs_h_distance').build(sample_ts).edges
+    out_got = ts2vg.NaturalVG(directed="top_to_bottom", weighted="abs_h_distance").build(sample_ts).edges
 
     out_truth = [
-        (1, 0, approx(1.)),
-        (1, 2, approx(1.)),
-        (1, 3, approx(2.)),
-        (2, 3, approx(1.)),
+        (1, 0, approx(1.0)),
+        (1, 2, approx(1.0)),
+        (1, 3, approx(2.0)),
+        (2, 3, approx(1.0)),
     ]
 
     assert sorted(out_got) == sorted(out_truth)
 
 
 def test_top_to_bottom_slope(sample_ts):
-    out_got = ts2vg.NaturalVG(directed='top_to_bottom', weighted='slope').build(sample_ts).edges
+    out_got = ts2vg.NaturalVG(directed="top_to_bottom", weighted="slope").build(sample_ts).edges
 
     out_truth = [
-        (1, 0, approx(1.)),
-        (1, 2, approx(-2.)),
+        (1, 0, approx(1.0)),
+        (1, 2, approx(-2.0)),
         (1, 3, approx(-1.5)),
-        (2, 3, approx(-1.)),
+        (2, 3, approx(-1.0)),
     ]
 
     assert sorted(out_got) == sorted(out_truth)
 
 
 def test_top_to_bottom_abs_slope(sample_ts):
-    out_got = ts2vg.NaturalVG(directed='top_to_bottom', weighted='abs_slope').build(sample_ts).edges
+    out_got = ts2vg.NaturalVG(directed="top_to_bottom", weighted="abs_slope").build(sample_ts).edges
 
     out_truth = [
-        (1, 0, approx(1.)),
-        (1, 2, approx(2.)),
+        (1, 0, approx(1.0)),
+        (1, 2, approx(2.0)),
         (1, 3, approx(1.5)),
-        (2, 3, approx(1.)),
+        (2, 3, approx(1.0)),
     ]
 
     assert sorted(out_got) == sorted(out_truth)
 
 
 def test_top_to_bottom_angle(sample_ts):
-    out_got = ts2vg.NaturalVG(directed='top_to_bottom', weighted='angle').build(sample_ts).edges
+    out_got = ts2vg.NaturalVG(directed="top_to_bottom", weighted="angle").build(sample_ts).edges
 
     out_truth = [
-        (1, 0, approx(atan(1.))),
-        (1, 2, approx(atan(-2.))),
+        (1, 0, approx(atan(1.0))),
+        (1, 2, approx(atan(-2.0))),
         (1, 3, approx(atan(-1.5))),
-        (2, 3, approx(atan(-1.))),
+        (2, 3, approx(atan(-1.0))),
     ]
 
     assert sorted(out_got) == sorted(out_truth)
 
 
 def test_top_to_bottom_abs_angle(sample_ts):
-    out_got = ts2vg.NaturalVG(directed='top_to_bottom', weighted='abs_angle').build(sample_ts).edges
+    out_got = ts2vg.NaturalVG(directed="top_to_bottom", weighted="abs_angle").build(sample_ts).edges
 
     out_truth = [
-        (1, 0, approx(atan(1.))),
-        (1, 2, approx(atan(2.))),
+        (1, 0, approx(atan(1.0))),
+        (1, 2, approx(atan(2.0))),
         (1, 3, approx(atan(1.5))),
-        (2, 3, approx(atan(1.))),
+        (2, 3, approx(atan(1.0))),
     ]
 
     assert sorted(out_got) == sorted(out_truth)
 
 
 def test_adjacency_matrix(sample_ts):
-    out_got = ts2vg.NaturalVG().build(sample_ts).adjacency_matrix(triangle='upper')
+    out_got = ts2vg.NaturalVG().build(sample_ts).adjacency_matrix(triangle="upper")
 
     out_truth = [
         [0, 1, 0, 0],
@@ -351,7 +351,7 @@ def test_empty_ts(empty_ts):
 
 
 def test_with_xs(sample_ts):
-    xs = [0., 1., 2., 2.1]
+    xs = [0.0, 1.0, 2.0, 2.1]
 
     out_got = ts2vg.NaturalVG().build(sample_ts, xs=xs).edges
 
@@ -365,14 +365,21 @@ def test_with_xs(sample_ts):
 
 
 def test_with_incompatible_xs(sample_ts):
-    xs = [0., 1., 2., 3., 4., 5., 6.]
+    xs = [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0]
 
     with pytest.raises(ValueError):
         ts2vg.NaturalVG().build(sample_ts, xs=xs)
 
 
 def test_with_non_monotonic_increasing_xs(sample_ts):
-    xs = [0., 4., 2., 3.]
+    xs = [0.0, 4.0, 2.0, 3.0]
+
+    with pytest.raises(ValueError):
+        ts2vg.NaturalVG().build(sample_ts, xs=xs)
+
+
+def test_with_non_monotonic_increasing_xs_2(sample_ts):
+    xs = [0.0, 0.0, 2.0, 3.0]
 
     with pytest.raises(ValueError):
         ts2vg.NaturalVG().build(sample_ts, xs=xs)
