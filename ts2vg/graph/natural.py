accb35d009f167339396fc477c75017bca2c36c3
Carlos Bergillos <carlos.bergillos.varela@est.fib.upc.edu>
2023-02-05 20:07:45 +0200

    Add missing specific graph type name

--- ts2vg/graph/natural.py
+++ ts2vg/graph/natural.py
@@ -56,6 +56,8 @@ class NaturalVG(BaseVG):
         edges = g.edges
     """
 
+    general_type_name = "Natural Visibility Graph"
+
     # def __init__(self, *args, **kwargs):
     #     super().__init__(*args, **kwargs)
 
