aee2f3e5bfc46900680d39c0e456ccda350a8aaf
Carlos Bergillos <carlos.bergillos.varela@est.fib.upc.edu>
2023-02-05 20:51:01 +0200

    Overhaul make_figures docs script

--- docs/source/examples/make_figures.py
+++ docs/source/examples/make_figures.py
@@ -1,39 +1,54 @@
-from basic import ax1, ax2, fig
+from pathlib import Path
 
-ax1.margins(x=0, y=0)
-ax2.margins(x=0, y=0)
-fig.tight_layout(w_pad=3)
-fig.savefig("basic.png", facecolor=(0, 0, 0, 0), bbox_inches="tight")
+basic_filepath = Path("basic.png")
+degree_distribution_filepath = Path("degree_distribution.svg")
+directed_filepath = Path("directed.svg")
+weighted_filepath = Path("weighted.svg")
+horizontal_filepath = Path("horizontal.svg")
+partitioning_filepath = Path("partitioning.png")
 
+if not basic_filepath.exists():
+    from basic import ax1, ax2, fig
 
-from degree_distribution import fig
-
-fig.tight_layout(w_pad=2)
-fig.savefig("degree_distribution.svg", facecolor=(0, 0, 0, 0), bbox_inches="tight")
-
-
-from directed import fig
-
-fig.tight_layout(w_pad=2)
-fig.savefig("directed.svg", facecolor=(0, 0, 0, 0), bbox_inches="tight")
+    ax1.margins(x=0, y=0)
+    ax2.margins(x=0, y=0)
+    fig.tight_layout(w_pad=3)
+    fig.savefig(basic_filepath, facecolor=(0, 0, 0, 0), bbox_inches="tight")
 
 
-from weighted import fig
+if not degree_distribution_filepath.exists():
+    from degree_distribution import fig
 
-fig.tight_layout(w_pad=2)
-fig.subplots_adjust(right=0.94)
-fig.savefig("weighted.svg", facecolor=(0, 0, 0, 0), bbox_inches="tight")
-
-
-from horizontal import fig
-
-fig.tight_layout(w_pad=2)
-fig.savefig("horizontal.svg", facecolor=(0, 0, 0, 0), bbox_inches="tight")
+    fig.tight_layout(w_pad=2)
+    fig.savefig(degree_distribution_filepath, facecolor=(0, 0, 0, 0), bbox_inches="tight")
 
 
-from partitioning import ax1, ax2, fig
+if not directed_filepath.exists():
+    from directed import fig
 
-ax1.margins(x=0, y=0)
-ax2.margins(x=0, y=0)
-fig.tight_layout(w_pad=3)
-fig.savefig("partitioning.png", facecolor=(0, 0, 0, 0), bbox_inches="tight")
+    fig.tight_layout(w_pad=2)
+    fig.savefig(directed_filepath, facecolor=(0, 0, 0, 0), bbox_inches="tight")
+
+
+if not weighted_filepath.exists():
+    from weighted import fig
+
+    fig.tight_layout(w_pad=2)
+    fig.subplots_adjust(right=0.94)
+    fig.savefig(weighted_filepath, facecolor=(0, 0, 0, 0), bbox_inches="tight")
+
+
+if not horizontal_filepath.exists():
+    from horizontal import fig
+
+    fig.tight_layout(w_pad=2)
+    fig.savefig(horizontal_filepath, facecolor=(0, 0, 0, 0), bbox_inches="tight")
+
+
+if not partitioning_filepath.exists():
+    from partitioning import ax1, ax2, fig
+
+    ax1.margins(x=0, y=0)
+    ax2.margins(x=0, y=0)
+    fig.tight_layout(w_pad=3)
+    fig.savefig(partitioning_filepath, facecolor=(0, 0, 0, 0), bbox_inches="tight")
