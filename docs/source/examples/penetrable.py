8d6444a693d5b0ab297e9adea5636576d5bcb6f5
Carlos Bergillos <carlos.bergillos.varela@est.fib.upc.edu>
2023-02-06 02:08:06 +0200

    Add horizontal visibility graph plot in docs

--- docs/source/examples/penetrable.py
+++ docs/source/examples/penetrable.py
@@ -1,13 +1,14 @@
-from plot_utils import plot_graph_demo
+from plot_utils import plot_graph_demo, plot_horizontal_graph_demo
 
 # ~~
-from ts2vg import NaturalVG
+from ts2vg import NaturalVG, HorizontalVG
 import matplotlib.pyplot as plt
 
 
 ts = [6.0, 3.0, 1.8, 4.2, 6.0, 3.0, 1.8, 4.8]
 
-vg_default = NaturalVG().build(ts)
+nvg_default = NaturalVG().build(ts)
+hvg_default = HorizontalVG().build(ts)
 
 penetrable_limit_options = [
     0,
@@ -15,12 +16,22 @@ penetrable_limit_options = [
     2,
 ]
 
-fig, axs = plt.subplots(ncols=3, figsize=(12, 3.5))
+fig, axs = plt.subplots(ncols=3, nrows=2, figsize=(12, 7))
 
-for penetrable_limit, ax in zip(penetrable_limit_options, axs):
-    ax.set_title(f"penetrable_limit={penetrable_limit}")
+# plot limited penetrable visibility graphs
+for penetrable_limit, ax in zip(penetrable_limit_options, axs.flat[:3]):
+    ax.set_title(f"NVG, penetrable_limit={penetrable_limit}")
 
-    vg = NaturalVG(penetrable_limit=penetrable_limit).build(ts)
-    added_edges = set(vg.edges) - set(vg_default.edges)
+    nvg = NaturalVG(penetrable_limit=penetrable_limit).build(ts)
+    added_edges = set(nvg.edges) - set(nvg_default.edges)
 
-    plot_graph_demo(vg, marked_edges=added_edges, ax=ax)
+    plot_graph_demo(nvg, marked_edges=added_edges, ax=ax)
+
+# plot limited penetrable horizontal visibility graphs
+for penetrable_limit, ax in zip(penetrable_limit_options, axs.flat[3:]):
+    ax.set_title(f"HVG, penetrable_limit={penetrable_limit}")
+
+    hvg = HorizontalVG(penetrable_limit=penetrable_limit).build(ts)
+    added_edges = set(hvg.edges) - set(hvg_default.edges)
+
+    plot_horizontal_graph_demo(hvg, marked_edges=added_edges, ax=ax)
--- docs/source/examples/plot_utils.py
+++ docs/source/examples/plot_utils.py
@@ -40,4 +40,50 @@ def plot_graph_demo(
 from matplotlib.patches import ArrowStyle, FancyArrowPatch
 
 
+def plot_horizontal_graph_demo(
+    vg,
+    ax,
+    edge_color=(0.25, 0.25, 0.25, 0.7),
+    marked_edge_color=(1.0, 0.35, 0.35, 0.7),
+    marked_edges=None,
+    bar_width=0.3,
+):
+    occupied_heights = set()  # used for naive overlap prevention
+
+    bars = ax.bar(vg.xs, vg.ts, color="#ccc", edgecolor="#000", width=bar_width)
+    ax.set_xticks(vg.xs)
+
+    for i, (n1, n2) in enumerate(vg.edges):
+        if marked_edges is not None and (n1, n2) in marked_edges:
+            color = marked_edge_color
+        else:
+            color = edge_color
+
+        y = min(vg.ts[n1], vg.ts[n2])
+
+        while round(y, 2) in occupied_heights:
+            y -= 0.18
+
+        occupied_heights.add(round(y, 2))
+
+        if n1 < n2:
+            x1, y1 = vg.xs[n1] + bar_width / 2, y
+            x2, y2 = vg.xs[n2] - bar_width / 2, y
+        else:
+            x1, y1 = vg.xs[n1] - bar_width / 2, y
+            x2, y2 = vg.xs[n2] + bar_width / 2, y
+
+        arrow = FancyArrowPatch(
+            (x1, y1),
+            (x2, y2),
+            arrowstyle=ArrowStyle("<|-|>", head_length=6, head_width=2.5),
+            shrinkA=0,
+            shrinkB=0,
+            color=color,
+            linewidth=2,
+        )
+
+        ax.add_patch(arrow)
+
+
 # $$
