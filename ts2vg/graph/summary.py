c0d6278f32d4f25eff227debe2a2322b0113ddf3
CarlosBergillos <carlos98.air.cn@gmail.com>
2023-09-13 20:55:06 +0200

    Add types to summary.py

--- ts2vg/graph/summary.py
+++ ts2vg/graph/summary.py
@@ -1,4 +1,10 @@
-def _simple_title_line(title, line_width):
+from typing import Any, Dict, TYPE_CHECKING, cast
+
+if TYPE_CHECKING:
+    from ts2vg.graph.base import VG
+
+
+def _simple_title_line(title: str, line_width: int) -> str:
     line = " " * ((line_width - len(title)) // 2)
     line += title
     line += " " * ((line_width - len(title)) // 2)
@@ -8,7 +14,7 @@ def _simple_title_line(title, line_width):
     return line
 
 
-def _simple_key_value_line(key, value, line_width):
+def _simple_key_value_line(key: Any, value: str, line_width: int) -> str:
     key = str(key)
 
     value = str(value)
@@ -21,8 +27,10 @@ def _simple_key_value_line(key, value, line_width):
     return line
 
 
-def simple_summary(vg: "ts2vg.graph.base.VG", title: str = "Visibility Graph", line_width: int = 48):
-    vg_config = {
+def simple_summary(vg: "VG", title: str = "Visibility Graph", line_width: int = 48) -> str:
+    vg._validate_is_built()
+
+    vg_config: Dict[str, Any] = {
         "General Type:": vg._general_type_name,
         "Directed:": vg.directed if vg.is_directed else "undirected",
         "Weighted:": vg.weighted if vg.is_weighted else "unweighted",
@@ -31,8 +39,8 @@ def simple_summary(vg: "ts2vg.graph.base.VG", title: str = "Visibility Graph", l
         "Penetrable Limit:": vg.penetrable_limit,
     }
 
-    built_vg_config = {
-        "Time Series Length:": len(vg.ts),
+    built_vg_config: Dict[str, Any] = {
+        "Time Series Length:": vg.n_vertices,
         "No. Vertices:": vg.n_vertices,
         "No. Edges:": vg.n_edges,
     }
